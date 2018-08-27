c6ffcbc1acb29b3cdccfe1508b44181b304fb1dc
julie <juliewang42@gmail.com>
2018-08-26 20:02:17 -0400

    set up home page

--- PacaTimer/MainPage.xaml.cs
+++ PacaTimer/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace PacaTimer
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 100));
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 500);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
         }
     }
 }
